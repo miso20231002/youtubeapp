0abe191c75fede4960d87ca328c7de6be0c2c7d5
miso20231002 <ohsung177@sungkyul.ac.kr>
2025-05-16 20:07:23 +0900

    06레이아웃세팅

---
+++ src/components/section/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header id='header' role='banner'>
+            <nav>
+        <a href="/">Home</a>
+        <a href="/today">Today</a>
+        <a href="/developer">Developer</a>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
